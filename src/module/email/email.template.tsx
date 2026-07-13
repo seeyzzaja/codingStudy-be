08c8036775c429e92978e618c781b4e0763efe01
seeyzzaja <seeyzzaja@gmail.com>
2026-07-13 23:39:40 +0700

    fix(email): correct import path for RegisterOtpEmail template

--- src/module/email/email.template.tsx
+++ src/module/email/email.template.tsx
@@ -1,6 +1,6 @@
 // import React from "react";
 import { render } from "@react-email/render";
-import RegisterOtpEmail from "./templates/register-otp";
+import RegisterOtpEmail from "./templates/register-otp.js";
 
 export const renderRegisterOtpEmail = async (
   name: string,
